f24641dd0b22bfcf101b0c33c1028dc112848660
amidabrian51 <briankaty1@me.com>
2019-12-03 21:55:15 +0000

    Added validation function

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-function validate() {
-    alert("This is working!")
+// function validate() {
+//     var input = document.createElement('input');
+//     input.type='email';
+//      input.value=document.getElementById('test').value;
+    
+//   if (input.checkValidity()) {
+//     document.getElementById('a').style.background = 'green';
+//   } else {
+//     document.getElementById('a').style.background = 'red';
+//   }
+    
+//   return false;
+// }
+
+
+function ValidateEmail(inputText) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputText.value.match(mailformat)) {
+    alert("You have entered a correct email addresss")
+    document.form1.text1.focus();
+    return true;
+  } else {
+    alert("You have entered an invalid email address!");
+    document.form1.text1.focus();
+    return false;
+  }
 }
