341d55e3fe2669cb2cdb4028e5185f2b3bee70ab
amidabrian51 <briankaty1@me.com>
2019-12-05 22:38:39 +0000

    Trying to append an image to the form.

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ function ValidateEmail(inputText) {
     document.form1.text1.focus();
     return true;
   } else {
+    var img = document.createElement('img');
+    img.src = "/images/icon-error.svg"
     document.getElementById("addedText").innerHTML += "This is the wrong email address";
+    document.getElementById("test").append(img);
     // alert("You have entered an invalid email address!");
     document.form1.text1.focus();
     return false;
