8c10b1d900209732093ae6c2d68702424dfcf0c9
amidabrian51 <briankaty1@me.com>
2019-12-07 22:50:14 +0000

    Still no solution to invalid email

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function ValidateEmail(inputText) {
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (inputText.value.match(mailformat)) {
-    alert("You have entered a correct email addresss")
-    document.form1.text1.focus();
-    return true;
-  } else {
-    // var img = document.createElement('img');
-    // img.src = "/images/icon-error.svg"
-    document.getElementById("addedText").innerHTML += "This is the wrong email address";
-    document.getElementById("test").classList.add("invalidEmail")
-    // alert("You have entered an invalid email address!");
-    document.form1.text1.focus();
-    return false;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (inputText.value.match(mailformat)) {
+      alert("You have entered a correct email addresss")
+      document.form1.text1.focus();
+      document.getElementById("test").classList.remove("invalidEmail")
+      return true;
+    } else {
+      // var img = document.createElement('img');
+      // img.src = "/images/icon-error.svg"
+      document.getElementById("addedText").innerHTML = "This is the wrong email address";
+      document.getElementById("test").classList.add("invalidEmail")
+      // alert("You have entered an invalid email address!");
+      document.form1.text1.focus();
+      return false;
+    }
   }
-}
