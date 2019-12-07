3c92ab3f1be9775591ddae3ed030928b437fa0ad
amidabrian51 <briankaty1@me.com>
2019-12-07 15:18:48 +0000

    reverted back to javascript

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// function ValidateEmail(inputText) {
-//   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-//   if (inputText.value.match(mailformat)) {
-//     alert("You have entered a correct email addresss")
-//     document.form1.text1.focus();
-//     return true;
-//   } else {
-//     var img = document.createElement('img');
-//     img.src = "/images/icon-error.svg"
-//     document.getElementById("addedText").innerHTML += "This is the wrong email address";
-//     document.getElementById("test").append(img);
-//     // alert("You have entered an invalid email address!");
-//     document.form1.text1.focus();
-//     return false;
-//   }
-// }
+function ValidateEmail(inputText) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputText.value.match(mailformat)) {
+    alert("You have entered a correct email addresss")
+    document.form1.text1.focus();
+    return true;
+  } else {
+    // var img = document.createElement('img');
+    // img.src = "/images/icon-error.svg"
+    document.getElementById("addedText").innerHTML += "This is the wrong email address";
+    document.getElementsByClassName("invalidEmail")
+    // alert("You have entered an invalid email address!");
+    document.form1.text1.focus();
+    return false;
+  }
+}
