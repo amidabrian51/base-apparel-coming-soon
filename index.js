1bad2d552b8cfc1b8c932209b0f7658ac738e503
amidabrian51 <briankaty1@me.com>
2019-12-08 11:40:57 +0000

    changed regular expression

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function ValidateEmail(inputText) {
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var mailformat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (inputText.value.match(mailformat)) {
       alert("You have entered a correct email addresss")
       document.form1.text1.focus();
